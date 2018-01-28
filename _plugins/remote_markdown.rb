07b79e4290ccca28c3b99564c15e6e52776044c7
Samuel Goldszmidt <samuel.goldszmidt@gmail.com>
2018-01-28 23:00:56 +0100

    Update remote_markdown.rb

--- _plugins/remote_markdown.rb
+++ _plugins/remote_markdown.rb
@@ -1,46 +1,24 @@
-# "THE BEER-WARE LICENSE" (Revision 42):
-# <robin.hahling@gw-computing.net> wrote this file. As long as you retain this
-# notice you can do whatever you want with this stuff. If we meet some day, and
-# you think this stuff is worth it, you can buy me a beer in return.
-# Robin Hahling
-
 require 'net/http'
+require 'uri'
 
 module Jekyll
-  # Remotely fetch a markdown file.
-  class RemoteMarkdownTag < Liquid::Tag
-    def initialize(tag_name, text, tokens)
+
+  class RemoteInclude < Liquid::Tag
+
+    def initialize(tag_name, remote_include, tokens)
       super
-
-      text.strip!
-      check_protocol(text)
-      uri = URI(text)
-
-      check_extension(uri.path)
-
-      res = Net::HTTP.get_response(uri)
-      fail 'resource unavailable' unless res.is_a?(Net::HTTPSuccess)
-
-      @content = res.body.force_encoding("UTF-8")
+      @remote_include = remote_include
     end
 
-    def render(_context)
-      @content
+    def open(url)
+      Net::HTTP.get(URI.parse(url.strip)).force_encoding 'utf-8'
     end
 
-    private
-
-    def check_protocol(text)
-      error_message = "remote_markdown: invalid URI given #{text}"
-      fail error_message unless text =~ URI.regexp(%w(http https ftp ftps))
+    def render(context)
+      open("#{@remote_include}")
     end
 
-    def check_extension(path)
-      mdexts = %w(.markdown .mkdown .mkdn .mkd .md)
-      error_message = "remote_markdown: URI file extension not in #{mdexts}"
-      fail error_message unless mdexts.include?(File.extname(path))
-    end
   end
 end
 
-Liquid::Template.register_tag('remote_markdown', Jekyll::RemoteMarkdownTag)
+Liquid::Template.register_tag('remote_include', Jekyll::RemoteInclude)
